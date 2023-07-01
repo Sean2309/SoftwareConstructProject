2e22a547bd65dea0a6e7450863bbd110f7931053
unknown <miyamii.x@gmail.com>
2023-07-01 19:03:40 +0800

    refactor: simple refactor to better differentiate renders

--- bank-app/client/src/components/LoyaltyPrograms.js
+++ bank-app/client/src/components/LoyaltyPrograms.js
@@ -19,21 +19,29 @@ class LoyaltyPrograms extends Component {
     componentDidMount() {
         this.getLoyaltyPrograms();
     }
+    
+    /* 
+        logic to pass on to actual render: if getLoyaltyPrograms is not yet successful,
+        render Loading... else pass each data to a LoyaltyProgram component
+    */ 
+    
+    renderLoyaltyPrograms() {
+        const { loyaltyProgramsData } = this.state;
+        
+        if (loyaltyProgramsData === undefined) {
+            return <p>Loading...</p>;
+        }
+        
+        return loyaltyProgramsData.map((data, index) => (
+            <LoyaltyProgram key={index} data={data} />
+        ));
+    }
 
     render() {
-        const { loyaltyProgramsData } = this.state;
-
         return (
             <div>
-                <h1>Loyalty Programs</h1>
-
-                {loyaltyProgramsData === undefined ? (
-                    <p>Loading...</p>
-                ) : (
-                    loyaltyProgramsData.map((data, index) => (
-                        <LoyaltyProgram key={index} data={data} />
-                    ))
-                )}
+                <h2>Loyalty Programs</h2>
+                {this.renderLoyaltyPrograms()}
             </div>
         );
     }
