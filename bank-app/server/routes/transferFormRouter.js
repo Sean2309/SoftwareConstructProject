aec824e50c245b6e361c4ea02dcba695f9bb37ad
unknown <miyamii.x@gmail.com>
2023-07-02 17:11:23 +0800

    chore: deleted old comments

--- bank-app/server/routes/transferFormRouter.js
+++ bank-app/server/routes/transferFormRouter.js
@@ -1,18 +1,6 @@
 const transferFormRouter = require('express').Router();
 const transferFormController = require('../controllers/transferFormController');
 
-/* 
-  sample data format
-  {
-    embershipId": "1021030213",
-      "memberName": "kei lowth
-    "transferDate", "23-10-2000"",
-    "transferAmount": "2000"    
-  }
-
-*/
-
-
 // Easy debug GET
 transferFormRouter.get('/', transferFormController.getAllForms);
 
