ee1bd78d65497c844e69ba23bd71e80e0629c7d3
Sean2309 <seanphay@gmail.com>
2023-07-03 15:45:52 +0800

    feat: downloading file from sftp server

--- transfer-connect-app/server/controllers/handbackFileController.js
+++ transfer-connect-app/server/controllers/handbackFileController.js
@@ -21,112 +21,184 @@ const Files = require('files.com/lib/Files').default;
 const File = require('files.com/lib/models/File').default;
 const { isBrowser } = require('files.com/lib/utils');
 
-// Datetime
+// // Datetime
 function getDate(date) {
   const day = date.getDate(); // Get the day (1-31)
   const month = date.getMonth() + 1; // Get the month (0-11), add 1 to match the human-readable month (1-12)
   const year = date.getFullYear(); // Get the four-digit year
-  const date_out = `${year}-${month}-${day}`;
+  const date_out = `${year}${month}${day}`;
   return date_out;
 }
-// Getting Current Date
+// // Getting Current Date
 const currentDate = getDate(new Date());
+console.log(`Current Date : ${currentDate}`);
 
 // Define Collections
 const collections = [`handback_test1`];
 
-// Creating the handback csv files
-const writeCollectionsToCsv = async() => {
-  mongoose.connect(process.env.MONGODB_URL1, { useNewUrlParser: true, useUnifiedTopology: true });
+// TODO
+// Workflow:
+1. 
 
-  // Finding the data to append
-  for (const collection of collections) {
-    const Model = mongoose.model(collection, handbackFileFormSchema);
-    const current_date1 = `2023-07-03T00:00:00.000+00:00`
-    const filter = { transferDate: current_date1 };
-    try {
 
-      const csvWriter = createCsvWriter({
-        path: `${collection}_HANDBACK_${currentDate}.csv`,
-        header: [
-          {id: `transferDate`, title: `Transfer Date`},
-          {id: `amount`, title: `Amount`},
-          {id: `referenceNumber`, title: `Reference number`},
-          {id: `outcomeCode`, title: `Outcome Code`}
-        ],
-      });
+
+const retrieveFromServer = async() => {
+
+  const transactionSchema = new mongoose.Schema({
+    membershipId: String,
+    membershipName: String,
+    transferDate: String,
+    transferAmount: Number,
+    referenceNumber: String,
+    partnerCode: String,
+    outcomeCode: String
+  });
+
+  const lp_list = [`DBSSG`];
+  const date1 = `20200812`;
+
+  for (const lp of lp_list ) {
+    Files.setBaseUrl('https://kaligo.files.com');
+    Files.setApiKey('d823bcf8852f7259262f425a839a05f88f51fa57e9cddb8c3d1493d10c04192e');
+      const filePath = `/transfer_connect_sutd_case_study_2023/c4i1/Handback/${lp}/${lp}_HANDBACK_${date1}.csv`
+      console.log(filePath);
+      const foundFile = await File.find(filePath);
+      const downloadableFile = await foundFile.download();
+
+      // for (const collection of collections) {
+      if (!isBrowser()) {
+        // download to a file on disk
+        await downloadableFile.downloadToFile(`./out/test_out.csv`);
+      }
+    // };
+  }
     
-      csvWriter.writeRecords([]);
+  
+  // const csvWriter = createCsvWriter({
+  //   path: `AL_HANDBACK.csv`,
+  //   header: [
+  //     {id: 'memberID', title: 'Member ID'},
+  //     {id: 'memberFirstName', title: 'Member first name'},
+  //     {id: 'memberLastName', title: 'Member last name'},
+  //     {id: 'transferDate', title: 'Transfer date'},
+  //     {id: 'amount', title: 'Amount'},
+  //     {id: 'referenceNumber', title: 'Reference number'},
+  //     {id: 'partnerCode', title: 'Partner code'},
+  //     {id: `outcomeCode`, title: `Outcome Code`}
+  //   ],
+  // });
+  //  const outcomeCode_list = [`0000`, `0001`, `0002`, `0003`, `0004`, `0005`, `0099`]
+  //  const gen_outcomeCode = outcomeCode_list[Math.floor(Math.random() * items.length)];
 
-      const data = await Model.find(filter);
-      const newData = data.map(doc => doc.toObject());
-      console.log("Data Retrieved from " + collection + ": ", newData);
-      
-      const csvWriter1 = createCsvWriter({
-        path: `${collection}_HANDBACK_${currentDate}.csv`,
-        header: [
-          {id: `transferDate`, title: `Transfer Date`},
-          {id: `amount`, title: `Amount`},
-          {id: `referenceNumber`, title: `Reference Number`},
-          {id: `outcomeCode`, title: `Outcome Code`}
-        ],
-        append: true
-      });      
+  //  // Writing to the csv file
+  //   await csvWriter.writeRecords()
 
-      // TODO: randomise outcome code?
-
-      await csvWriter1.writeRecords(newData);
-
-      console.log(`Data written to ${currentDate}_handback_out.csv.`);
-
-      }
-      catch (error) {
-        console.error("An error occurred while handling collection " + collection + ": " + error);
-        }
+  // Write to the csv file
+  // await csvWriter.writeRecords()
   }
-  mongoose.connection.close();
-};
+    
 
-// Retrieving accrual file from server
-const uploadFilesToServer = async () => {
-  Files.setBaseUrl('https://kaligo.files.com');
-  Files.setApiKey('d823bcf8852f7259262f425a839a05f88f51fa57e9cddb8c3d1493d10c04192e');
+// Creating the handback csv files
+// const writeCollectionsToCsv = async() => {
+//   mongoose.connect(process.env.MONGODB_URL1, { useNewUrlParser: true, useUnifiedTopology: true });
 
-  for (const collection of collections) {
-    if (!isBrowser()) {
-      try { 
-        await File.uploadFile(`/transfer_connect_sutd_case_study_2023/c4i1/Handback/AL_HANDBACK_${collection}_${currentDate}.txt`, `${collection}_HANDBACK_${currentDate}.csv`);
-        console.log('File uploaded successfully.');
-      } catch (error) {
-        console.error('An error occurred while uploading file for collection ' + collection + ':', error);
-      }
-    } else {
-      console.log('File upload skipped because it is running in a browser environment.');
-    }
-  }
-};
+//   // Finding the data to append
+//   for (const collection of collections) {
+//     const Model = mongoose.model(collection, handbackFileFormSchema);
+//     const currDate = new Date()
+//     currDate.setHours(0,0,0,0);
 
-// Updating the handback file into DB
-const updateDB = async() => {
-  mongoose.connect(process.env.MONGODB_URL1, {useNewUrlParser: true, useUnifiedTopology: true} );
+//     console.log(currDate);
+//     console.log("##################")
+//     const current_date1 = `2023-07-03T00:00:00.000+00:00`
+//     console.log(`2023-07-03T00:00:00.000+00:00` == currDate);
+//     // TODO: Need to sync the uploaded data to the current date. Current format for the new Date() in js and uploaded date in Mongo DB is different
+//     const filter = { transferDate: current_date1 };
+//     try {
 
-  // Need to filter based on the loyalty program
-  for (const collection in collections) {
-    const Model = mongoose.model(collection, handbackFileFormSchema);
-    try {
+//       const csvWriter = createCsvWriter({
+//         path: `${collection}_HANDBACK_${currentDate}.csv`,
+//         header: [
+//           {id: `transferDate`, title: `Transfer Date`},
+//           {id: `amount`, title: `Amount`},
+//           {id: `referenceNumber`, title: `Reference number`},
+//           {id: `outcomeCode`, title: `Outcome Code`}
+//         ],
+//       });
+    
+//       csvWriter.writeRecords([]);
+
+//       const data = await Model.find(filter);
+//       const newData = data.map(doc => doc.toObject());
+//       console.log("Data Retrieved from " + collection + ": ", newData);
       
-    }
-    catch (err) {
-      console.log("Failed to upload to mongo db" + err);
-    }
-  };
-}
+//       const csvWriter1 = createCsvWriter({
+//         path: `${collection}_HANDBACK_${currentDate}.csv`,
+//         header: [
+//           {id: `transferDate`, title: `Transfer Date`},
+//           {id: `amount`, title: `Amount`},
+//           {id: `referenceNumber`, title: `Reference Number`},
+//           {id: `outcomeCode`, title: `Outcome Code`}
+//         ],
+//         append: true
+//       });
+
+//       // TODO: randomise outcome code?
+
+//       await csvWriter1.writeRecords(newData);
+
+//       console.log(`Data written to ${currentDate}_handback_out.csv.`);
+
+//       }
+//       catch (error) {
+//         console.error("An error occurred while handling collection " + collection + ": " + error);
+//         }
+//   }
+//   mongoose.connection.close();
+// };
+
+// const uploadFilesToServer = async () => {
+//   Files.setBaseUrl('https://kaligo.files.com');
+//   Files.setApiKey('d823bcf8852f7259262f425a839a05f88f51fa57e9cddb8c3d1493d10c04192e');
+
+//   for (const collection of collections) {
+//     if (!isBrowser()) {
+//       try { 
+//         await File.uploadFile(`/transfer_connect_sutd_case_study_2023/c4i1/Handback/AL_HANDBACK_${collection}_${currentDate}.txt`, `${collection}_HANDBACK_${currentDate}.csv`);
+//         console.log('File uploaded successfully.');
+//       } catch (error) {
+//         console.error('An error occurred while uploading file for collection ' + collection + ':', error);
+//       }
+//     } else {
+//       console.log('File upload skipped because it is running in a browser environment.');
+//     }
+//   }
+// };
+
+// // Updating the handback file into DB
+// const updateDB = async() => {
+//   mongoose.connect(process.env.MONGODB_URL1, {useNewUrlParser: true, useUnifiedTopology: true} );
+
+//   // Need to filter based on the loyalty program
+//   for (const collection in collections) {
+//     const Model = mongoose.model(collection, handbackFileFormSchema);
+//     try {
+      
+//     }
+//     catch (err) {
+//       console.log("Failed to upload to mongo db" + err);
+//     }
+//   };
+// }
+
+
 
 
 // Running the functions
 const main = async () => {
-  await writeCollectionsToCsv();
-  await uploadFilesToServer();
+  // await writeCollectionsToCsv();
+  // await uploadFilesToServer();
+  await retrieveFromServer();
   console.log("Done!");
 }
 
