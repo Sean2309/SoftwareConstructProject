42ac3d1d0295a85297300d8b34cf61987f81d6e9
Daniel Yuen <daniel_yuen@mymail.sutd.edu.sg>
2023-07-04 02:41:50 +0800

    Buggy Controller
    Buggy controller but synced to 2 collections

--- transfer-connect-app/server/controllers/loyaltyProgramQueryController.js
+++ transfer-connect-app/server/controllers/loyaltyProgramQueryController.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const loyaltyProgramQueryController = express.Router();
 const loyaltyProgramQueryModel = require('../models/loyaltyProgramQueryModel');
 const currencyRateModel = require('../models/currencyRateModel');
 
@@ -11,7 +13,6 @@ const currencyRateModel = require('../models/currencyRateModel');
   description: "YOL"
   enrollmentLink: "https://www.gojet.com/member/"
   tncLink: "https://www.gojet.com/aa/about-us/en/gb/terms-and-conditions.html"
-  format: "9digits1letter"
   }
 
     sample data format :CurrencyRate Model 
@@ -22,38 +23,77 @@ const currencyRateModel = require('../models/currencyRateModel');
 
 */
 
-loyaltyProgramQueryController.get('/LPPdata', async(req,res) => {
+// loyaltyProgramQueryController.get('/LPPdata', async(req,res) => {
+loyaltyProgramQueryController.get('/:programID', async(req,res) => {
   try {
-    const data = await loyaltyProgramQueryModel.find({});
+    // const data = await loyaltyProgramQueryModel.find({});
+    const { programID } = req.params;
+    
+    // convert documents to an array 
+    const loyaltyProgramProviders = await loyaltyProgramQueryModel.find({programID});
+    const currencyRates = await currencyRateModel.find({programID});
+
+    // Combine the data from both collections based on the programID field
+    const combinedData = loyaltyProgramProviders.map(program => {
+    const currencyRate = currencyRates.find(rate => rate.programID === program.programID);
+    const transformedDocument={
+      programID: program.programID,
+      programName: program.programName,
+      currencyName: program.currencyName,
+      processingTime: program.processingTime,
+      description: program.description,
+      enrollmentLink: program.enrollmentLink,
+      tncLink: program.tncLink,
+      membershipFormat: program.membershipFormat,
+      currencyRate: currencyRate ? currencyRate.currencyRate : null
+    };
+    // if (program._id) {
+    //   transformedDocument.id = program._id.toString();
+    // }
+    
+    // return transformedDocument;
+    });
 
      // Transform the data before sending the response
-     const transformedData = data.map(document => {
-      const transformedDocument = { ...document._doc };
-      transformedDocument.id = transformedDocument._id.toString();
+     const transformedData = combinedData.map(document => {
+      const transformedDocument = { ...document };
+      // if (transformedDocument._id) {
+      //   transformedDocument.id = transformedDocument._id.toString();
+      //   delete transformedDocument._id;
+      // }
       delete transformedDocument._id;
-      delete transformedDocument.__v;
-      delete transformedDocument.id;
+      // delete transformedDocument.__v;
       return transformedDocument;
     });
+
+    //  // Transform the data before sending the response
+    //  const transformedData = data.map(document => {
+    //   const transformedDocument = { ...document._doc };
+    //   transformedDocument.id = transformedDocument._id.toString();
+    //   delete transformedDocument._id;
+    //   delete transformedDocument.__v;
+    //   delete transformedDocument.id;
+    //   return transformedDocument;
+    // });
     
     console.log(transformedData); // Output transformed data to the terminal
     res.status(200).json(transformedData); // convert 'transformedData' to JSON string and send as the response body , along with HTTP status 
 
-    }
-    catch (error) {
-      response.status(500).json({ message: error.message })
-    }
-  }
 }
-
-const loyaltyProgramQueryController = new LoyaltyProgramQueryController();
-
+  catch (error){
+    res.status(500).json({message:error.message})
+  }
+})
 
 
 module.exports = loyaltyProgramQueryController;
 
-// -------------------- ADDITION TO DB ------------ //
-// Insert new LoyaltyProgramProvider into Db
+
+
+
+
+// -------------------- ADDITION TO DB ------------ // 
+// Insert new LoyaltyProgramProvider into Db 
 // createLoyaltyProgramProvider = async (request, response) => {
 
 //   try {
@@ -75,7 +115,7 @@ module.exports = loyaltyProgramQueryController;
 
 //     //Succesful update
 //     console.log(`Loyalty Program Provider successfully added to Db`)
-//     res.sendStatus(201);
+//     res.sendStatus(201); 
 //     provider.mongoose.connection.close()
 
 //   } catch (error) {
--- transfer-connect-app/server/index.js
+++ transfer-connect-app/server/index.js
@@ -4,19 +4,29 @@ const config = require('./utils/config');
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const loyaltyProgramQueryRouter = require("./routes/loyaltyProgramQueryRouter")
+const transferconnect_LPQ = require('./controllers/loyaltyProgramQueryController');
+
+
 
 const app = express();
 
 // connect to mongoDB cloud
 mongoose.set("strictQuery",false)
-mongoose.connect('mongodb+srv://danielyuen:1234@test.j9ugyp5.mongodb.net/testdb?retryWrites=true&w=majority')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+// mongoose.connect('mongodb+srv://danielyuen:1234@test.j9ugyp5.mongodb.net/testdb?retryWrites=true&w=majority')
+//   .then(() => {
+//     console.log('Connected to MongoDB');
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to MongoDB:', error);
+//   });
+
+mongoose.connect('mongodb+srv://Daniel:vLfmoyUKJ3Fy8dzx@transferconnect.0papjri.mongodb.net/TransferConnectDB?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 
 // enable CORS for all routes
@@ -26,9 +36,12 @@ app.use(cors());
 // for purpose of parsing incoming requests 
 app.use(express.json());
 
-app.use('/api/loyaltyprograms', loyaltyProgramQueryRouter)
+
+app.use('/api/loyaltyprograms', transferconnect_LPQ)
   
 
 app.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`);
 })
+
+console.log(`hi`)
