c09e557b4a3932ef01fa82c1e4a1a202f1eff86e
Leelexuan <leelxuan@gmail.com>
2023-07-05 22:00:24 +0800

    deprecated: removed unused add

--- transfer-connect-app/server/controllers/transactionEnquiryController.js
+++ transfer-connect-app/server/controllers/transactionEnquiryController.js
@@ -36,13 +36,13 @@ async function getOutcomeCode(collection_connection, id_list){
   console.log(id_list);
   let outcomeCodes = [];
   //use of instead of in - in makes 0000 into 0 
-  for (id of id_list){
+  for (let id of id_list){
     console.log(id);
     await collection_connection.find({"referenceNumber": id}, {"outcomeCode": 1, "referenceNumber": 1, "_id": 0 })
     .then(user => {
       if (user) {
         console.log('Found transactions:', user);
-        outcomeCodes = user;
+        outcomeCodes.push(user[0]);
 
       } else {
         console.log('Transactions not found');
@@ -53,47 +53,6 @@ async function getOutcomeCode(collection_connection, id_list){
   });}
   return outcomeCodes;
   };
-
-async function processRouteAdd(req){
-  const id = req.params;
-  if (id == null){
-      return;
-  }
-  console.log(id.loyalty_program);
-  console.log(id.bank_app);
-
-  //connections to specific DB and collection
-  var bank_name = id.bank_app;
-  var loyalty_program_name = id.loyalty_program;
-  const database_connection = mongoose.connection.useDb(loyalty_program_name);
-  const collection_connection = database_connection.model(bank_name, transactionSchema, bank_name);
-  submitTransaction(collection_connection);
-
-  return;
-};
-
-
-//add transaction (for testing)
-async function submitTransaction(collection_connection){
-    const transactionData = {
-      "membershipId": "1230oij",
-      "membershipName": "test_Data",
-      "transferDate": "2020-01-01",
-      "transferAmount": 10000,
-      "referenceNumber": "1111",
-      "partnerCode": "DBS",
-      "outcomeCode": "00"
-      };
-    const transaction1 = new collection_connection(transactionData);
-    console.log(transaction1);
-    transaction1.save()
-      .then(() => {
-        console.log('Transfer form data saved to MongoDB');
-      })
-      .catch((error) => {
-        console.error('Error saving transfer form data:', error);
-      });
   
-    };
 
 module.exports = {processRoute, processRouteAdd};
--- transfer-connect-app/server/routes/transactionEnquiryRouter.js
+++ transfer-connect-app/server/routes/transactionEnquiryRouter.js
@@ -2,10 +2,6 @@ const express = require('express');
 const transactionmodel = require('../controllers/transactionEnquiryController.js');
 var router = express.Router();
 
-//for testing purposes only
-router.get('/add/:bank_app/:loyalty_program', async function(req, res, next) {
-    transactionmodel.processRouteAdd(req);
-});
 
 
 router.get('/check/:bank_app/:loyalty_program/:referencenumber', async function(req, res, next) {
